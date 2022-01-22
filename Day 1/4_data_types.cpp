d9be3af93b7e473ff0ff561734fe2892b0086920
garg-anubhav <anubhavgarg182@gmail.com>
2022-01-22 16:45:24 +0530

    minor updation

--- Day 1/4_data_types.cpp
+++ Day 1/4_data_types.cpp
@@ -4,7 +4,7 @@ int main()
 {
     int age = 22;
     string name = "Anubhav Garg";
-    char gender = "Male";
+    char gender = 'M';
     bool isMarried = false;
     float weight = 75.5;
     cout << name << "\n";
